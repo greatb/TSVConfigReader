75d705e75da540b7e711d6d75b96e2fcfffd37d2
Bala Thavapalan <bthavapalan@gmail.com>
2015-10-15 15:41:20 +1100

    Core file reading implemented

--- TSVConfig.cs
+++ TSVConfig.cs
@@ -1,16 +1,33 @@
-﻿using TSVConfigReader.Core;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TSVConfigReader.Core;
 
 namespace TSVConfigReader
 {
-    public static class BarConfig
+    public static class TSVConfig
     {
         private const string CACHE_KEY = "textConfigs";
 
         private static AppEnvironment appEnv;
 
+        private static string fileName = @"c:\temp\TextFile1.txt";
+
         public static string GetConfig(string key)
         {
-            return "";
+            string output = string.Empty;
+
+            Dictionary<string, string> textConfigs;
+
+            textConfigs = File
+                .ReadAllLines(fileName)
+                .Select(x => x.Split('\t'))
+                .Where(x => x.Length > 1)
+                .ToDictionary(x => x[0].Trim(), x => x[(int)appEnv]);
+
+
+            output = textConfigs[key];
+            return output;
         }
     }
 }
